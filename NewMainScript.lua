44fe1274bd896bf1bd190a917584d570deb53c46
dairuksa-cpu <dairuksa@gmail.com>
2026-06-29 22:09:20 +0300

    fix: preload game config from VapeCompiled before Vape loads, restore compiled game files as backup

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -60,6 +60,22 @@ if not shared.VapeDeveloper then
 	writefile('newvape/profiles/commit.txt', commit)
 end
 
+-- Pre-download the current game config from VapeCompiled so Vape finds it cached locally
+local function preloadGameConfig()
+	local pid = tostring(game.PlaceId)
+	if pid ~= '0' and not isfile('newvape/games/'..pid..'.lua') then
+		local commit = (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt')) or 'main'
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/7GrandDadPGN/VapeCompiled/'..commit..'/games/'..pid..'.lua', true)
+		end)
+		if suc and res and res ~= '404: Not Found' then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			writefile('newvape/games/'..pid..'.lua', res)
+		end
+	end
+end
+preloadGameConfig()
+
 -- Hook __namecall to block Kick with Bedwars message
 if hookmetamethod and getnamecallmethod then
 	local oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
